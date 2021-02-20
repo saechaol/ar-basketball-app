e888fb005a38543c32786031d7fb29962cbd8881
Lucas <33850575+saechaol@users.noreply.github.com>
2021-02-19 20:03:18 -0800

    Commit swift boilerplate to xcode

---
+++ hoops/hoops/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  hoops
+//
+//  Created by 세차오 루카스 on 2/19/21.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
