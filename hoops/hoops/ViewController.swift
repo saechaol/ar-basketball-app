c7eed31f8db8bebc48f3134c69fb6609fc7ba824
someguy13 <ryan.kwong@gmail.com>
2021-02-20 14:45:16 -0800

    added panning calculations

--- hoops/hoops/ViewController.swift
+++ hoops/hoops/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import RealityKit
+import ARKit
+import SceneKit
 
 class ViewController: UIViewController {
     
@@ -17,7 +19,7 @@ class ViewController: UIViewController {
         
         // create anchor entity
         // this object will provide a central 'tether' to keep everything in AR worldspace
-        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [1.0, 1.0]);
+        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.1, 0.1]);
         arView.scene.addAnchor(anchor);
         
         let worldObjects: [Entity] = loadBasketballMesh();
@@ -26,7 +28,14 @@ class ViewController: UIViewController {
             anchor.addChild(model);
         }
         
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(executePan))
+        view.addGestureRecognizer(pan)
+        
+        arView.debugOptions.insert(.showSceneUnderstanding)
+        
     }
+    
+    
     
     private func loadBasketballMesh() -> [Entity] {
         var worldObjects: [Entity] = [];
@@ -39,9 +48,11 @@ class ViewController: UIViewController {
         let basketballModel = ModelEntity(mesh: basketballMesh, materials: [basketballMaterial]);
         
         do {
+            
             let hoopModel: Entity;
             try hoopModel = ModelEntity.load(named: "basketball_hoop.usdz");
             worldObjects.append(hoopModel);
+ 
         } catch BasketballError.runtimeError("File Not Found") {
             print("Basketball hoop model not found");
         } catch {
@@ -51,13 +62,43 @@ class ViewController: UIViewController {
         
         worldObjects.append(basketballModel);
         
-        worldObjects[0].position = [0, 0, 0];
-        worldObjects[1].position = [5, 0, 5];
+        worldObjects[0].position = [0, 0, -200];
+        worldObjects[1].position = [0, 1, 0];
         
         return worldObjects;
     }
     
-    
+    @objc func executePan(gesture: UIPanGestureRecognizer){
+        
+        //print("Panning");
+        if(gesture.state == .began){
+            let start = gesture.location(in: view)
+            print("Beginning at:",start.x,start.y)
+            
+            //let hitResult = view.hitTest(start, options:[:])
+        }
+        else if(gesture.state == .cancelled){
+            print("Cancelled")
+        }
+        else if(gesture.state == .ended){
+            let velocity = gesture.velocity(in: view);
+            let x = Double(velocity.x);
+            let y = Double(velocity.y);
+            let end = gesture.location(in: view)
+            var angle = atan2(y,x) * 180.0/Double.pi;
+            
+            if(angle < 0) {
+                angle += 360.0;
+            }
+            print("Angle:",angle)
+            print("Velocity:",velocity)
+            print("Ending at:",end.x,end.y)
+        }
+        
+        
+        
+    }
+
 }
 
 enum BasketballError: Error {
