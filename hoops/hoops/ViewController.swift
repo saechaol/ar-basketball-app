ea8704e45de751cd6c89b81c2a14df2b4795d95b
Lucas <33850575+saechaol@users.noreply.github.com>
2021-02-20 21:14:34 -0800

    Spawn a new ball at the origin

--- hoops/hoops/ViewController.swift
+++ hoops/hoops/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     var worldObjects: [ModelEntity] = []
-    
+    var ballCount = 0;
     // renders to the ios display
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         // create anchor entity
         // this object will provide a central 'tether' to keep everything in AR worldspace
         let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.1, 0.1]); // 1 meters squared
+        anchor.name = "RootAnchor"
         arView.scene.addAnchor(anchor);
         arView.environment.sceneUnderstanding.options.insert(.physics);
      //   arView.environment.sceneUnderstanding.options.insert(.occlusion);
@@ -35,22 +36,21 @@ class ViewController: UIViewController {
         let pan = UIPanGestureRecognizer(target: self, action: #selector(executePan))
         view.addGestureRecognizer(pan)
         
+        let button = drawBallButton();
+        view.addSubview(button);
+        
     }
     
     
     private func loadBasketballMesh() -> [ModelEntity] {
         var worldObjects: [ModelEntity] = [];
         
-        // generate sphere mesh and mesh material
-        let basketballMesh = MeshResource.generateSphere(radius: 0.242);
-        let basketballMaterial = SimpleMaterial(color: .orange, isMetallic: false);
-        
-        // assign mesh to ModelEntity object
-        let basketballModel = ModelEntity(mesh: basketballMesh, materials: [basketballMaterial]);
+        let basketballModel = generateBall();
         do {
             let hoopModel: ModelEntity;
             try hoopModel = ModelEntity.loadModel(named: "basketball_hoop.usdz");
             let sizeScalar = SIMD3<Float>(0.05, 0.05, 0.05)
+            hoopModel.name = "Hoop";
             hoopModel.setScale(sizeScalar, relativeTo: nil);
             hoopModel.generateCollisionShapes(recursive: true);
             hoopModel.physicsBody = .init();
@@ -62,23 +62,38 @@ class ViewController: UIViewController {
             print("Unexpected errors");
         }
         
+        let groundPlane = generateGroundPlane()
+        
+        worldObjects.append(groundPlane);
+        worldObjects.append(basketballModel);
+        
+                                 // X   Y   Z
+        worldObjects[0].position = [0, 1, -10]; // hoop
+        worldObjects[1].position = [0, -2, -5]; // plane
+        worldObjects[2].position = [0, 1.5, -0.5]; // basketball
+        
+        return worldObjects;
+    }
+    
+    private func generateBall() -> ModelEntity {
+        // generate sphere mesh and mesh material
+        let basketballMesh = MeshResource.generateSphere(radius: 0.242);
+        let basketballMaterial = SimpleMaterial(color: .orange, isMetallic: false);
+        
+        // assign mesh to ModelEntity object
+        let basketballModel = ModelEntity(mesh: basketballMesh, materials: [basketballMaterial]);
+        
         // generate collision and physics object of ball
         basketballModel.generateCollisionShapes(recursive: true)
         basketballModel.physicsBody = .init();
         basketballModel.physicsBody?.mode = .dynamic;
         
-        let groundPlane = generateGroundPlane()
+        basketballModel.name = "Ball" + String(ballCount);
+        ballCount += 1;
         
-        worldObjects.append(basketballModel);
-        worldObjects.append(groundPlane);
-        
-                                 // X   Y   Z
-        worldObjects[0].position = [0, 1, -10]; // hoop
-        worldObjects[1].position = [0, 1.5, 0]; // basketball
-        worldObjects[2].position = [0, -2, -5]; // plane
-        
-        return worldObjects;
+        return basketballModel;
     }
+    
     @objc func executePan(gesture: UIPanGestureRecognizer){
         
         //print("Panning");
@@ -112,7 +127,7 @@ class ViewController: UIViewController {
         //arView.scene.
         print(arView.cameraTransform);
         let force = SIMD3<Float>.init(Float(velocity.x/1000),Float(sqrt(pow(velocity.x,2)+pow(velocity.y,2))/500),Float(velocity.y/1000));
-        worldObjects[1].applyLinearImpulse(force, relativeTo: nil);
+        worldObjects[2].applyLinearImpulse(force, relativeTo: nil);
         //print(worldObjects[1].physicsMotion.debugDescription);
         //print(worldObjects[1].physicsBody.debugDescription);
     }
@@ -126,9 +141,46 @@ class ViewController: UIViewController {
         groundPlaneModel.physicsBody = .init();
         groundPlaneModel.physicsBody?.mode = .static;
         
+        groundPlaneModel.name = "GroundPlane";
         
         return groundPlaneModel;
     }
+    
+    /*
+     * Returns a button labeled "Reset ball position"
+     */
+    func drawBallButton() -> UIButton {
+        let ballButton = UIButton();
+        let screenWidth = UIScreen.main.bounds.width;
+        let screenHeight = UIScreen.main.bounds.height;
+        ballButton.setTitle("Reset Ball Position", for: .normal);
+        ballButton.setTitleColor(UIColor.blue, for: .normal)
+        ballButton.frame = CGRect(x: 15, y: -50, width: 300, height: 500)
+        ballButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        ballButton.center = CGPoint(x: screenWidth / 2.0, y: screenHeight - 100);
+        
+        return ballButton;
+    }
+    
+    /*
+     * Generate a new ball and pass control to it.
+     * Remove the last ball from the scenegraph
+     */
+    @objc func buttonPressed(sender: UIButton!) {
+        print("Reset ball pressed")
+        
+        let ballName = "Ball" + String(ballCount - 1);
+        let ball = generateBall();
+        ball.position = [0, 1.5, -0.5]; // basketball
+        worldObjects[2] = ball; // set control to current ball
+        // print(arView.scene.anchors);
+        
+        let anchor = arView.scene.findEntity(named: "RootAnchor")
+        
+        anchor?.removeChild((anchor?.findEntity(named: ballName))!);
+        anchor?.addChild(ball);
+    }
+    
 }
 
 enum BasketballError: Error {
